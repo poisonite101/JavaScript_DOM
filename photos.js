8e6dd69cdb3fa8540d7080a59f8db85632564204
Zac <zacace@ingoglia.com>
2020-09-08 14:13:14 -0700

    09-08-2020 Update 1
    format all documents according to Prettier conventions

--- photos.js
+++ photos.js
@@ -11,46 +11,44 @@
 "use strict"; // interpret document contents in JavaScript strict mode
 
 /* global variables */
-var photoOrder = [1,2,3,4,5];
+var photoOrder = [1, 2, 3, 4, 5];
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
-   for (var i = 0; i < 5; i++) {
-      if ((photoOrder[i] + 1) === 6) {
-         photoOrder[i] = 1;
-      } else {
-         photoOrder[i] += 1;
-      }
-      populateFigures();
-   }
+  for (var i = 0; i < 5; i++) {
+    if (photoOrder[i] + 1 === 6) {
+      photoOrder[i] = 1;
+    } else {
+      photoOrder[i] += 1;
+    }
+    populateFigures();
+  }
 }
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
-   for (var i = 0; i < 5; i++) {
-      if ((photoOrder[i] - 1) === 0) {
-         photoOrder[i] = 5;
-      } else {
-         photoOrder[i] -= 1;
-      }
-      populateFigures();
-   }
+  for (var i = 0; i < 5; i++) {
+    if (photoOrder[i] - 1 === 0) {
+      photoOrder[i] = 5;
+    } else {
+      photoOrder[i] -= 1;
+    }
+    populateFigures();
+  }
 }
 
 /* open center figure in separate window */
-function zoomFig() {
-   
-}
+function zoomFig() {}
 
 /* create event listeners and populate image elements */
 function setUpPage() {
-   createEventListeners();
-   populateFigures();
+  createEventListeners();
+  populateFigures();
 }
 
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
-  window.addEventListener("load", setUpPage, false); 
-} else if (window.attachEvent)  {
+  window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
   window.attachEvent("onload", setUpPage);
 }
--- zoom.js
+++ zoom.js
@@ -16,7 +16,7 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 
 /* populate img element and create event listener */
 function pageSetup() {
-   document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+  document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
 }
 
 /* add img src value and create event listener when page finishes loading */
